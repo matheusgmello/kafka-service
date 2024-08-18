d6869e6ea9bedd9d37b489644168eea1627b436c
matheus mello <matheusgmello1@gmail.com>
2024-08-18 11:13:52 -0300

    feat: add callback function

--- str-producer/src/main/java/dev/matheus/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/dev/matheus/strproducer/services/StringProducerService.java
@@ -1,10 +1,11 @@
 package dev.matheus.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class StringProducerService {
@@ -12,6 +13,16 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message);
+        kafkaTemplate.send("str-topic", message).addCallback(
+                success -> {
+                    if(success != null) {
+                        log.info("Send message with success {}", message);
+                        log.info("Partition {}, Offset {}",
+                                success.getRecordMetadata().partition(),
+                                success.getRecordMetadata().offset());
+                    }
+                },
+                error -> log.error("Error send message")
+        );
     }
 }
