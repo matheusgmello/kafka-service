64fdfe59ce67987be7050f72a7f76646891a315f
matheus mello <matheusgmello1@gmail.com>
2024-08-20 11:47:21 -0300

    feat: add json consumer

---
+++ json-consumer/src/test/java/dev/matheus/jsonconsumer/JsonConsumerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.matheus.jsonconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonConsumerConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
